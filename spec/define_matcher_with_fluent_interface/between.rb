df569b752c7fc0ff40460f7c7dce6aedc12b261c
D.J. Crosby <taladan@gmail.com>
2023-05-22 12:28:52 -0400

    chained method with argument

---
+++ spec/define_matcher_with_fluent_interface/between.rb
@@ -0,0 +1,15 @@
+# between.rb
+
+RSpec::Matchers.define :be_bigger_than do |first|
+  match do |actual|
+    (actual > first) && (actual < @second)
+  end
+
+  chain :and_smaller_than do |second|
+    @second = second
+  end
+end
+
+RSpec.describe 5 do
+  it { is_expected.to be_bigger_than(4).and_smaller_than(6) }
+end
