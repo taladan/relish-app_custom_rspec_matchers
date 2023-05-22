34664e9cbd793672cc21696e187ae17d03ee59e4
D.J. Crosby <taladan@gmail.com>
2023-05-21 21:20:56 -0400

    Define a matcher with default messages

---
+++ spec/matcher_with_default_message_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec/expectations'
+
+RSpec::Matchers.define :be_a_multiple_of do |expected|
+  match do |actual|
+    actual % expected == 0
+  end
+end
+
+RSpec.describe 9 do
+  it { is_expected.to be_a_multiple_of(3) }
+end
+
+RSpec.describe 9 do
+  it { is_expected.not_to be_a_multiple_of(4) }
+end
+
+# fail intentionally to generate expected output
+RSpec.describe 9 do
+  it { is_expected.to be_a_multiple_of(4) }
+end
+
+# fail intentionally to generate expected output
+RSpec.describe 9 do
+  it { is_expected.not_to be_a_multiple_of(3) }
+end
