ca614f811e8c78c4738e498f75d2614e39ecb327
D.J. Crosby <taladan@gmail.com>
2023-05-22 12:35:37 -0400

    chained setter

---
+++ spec/define_matcher_with_fluent_interface/between_chained_setter_spec.rb
@@ -0,0 +1,13 @@
+# between_chained_setter_spec.rb
+
+RSpec::Matchers.define :be_bigger_than do |first|
+  match do |actual|
+    (actual > first) && (actual < second)
+  end
+
+  chain :and_smaller_than, :second
+end
+
+RSpec.describe 5 do
+  it { is_expected.to be_bigger_than(4).and_smaller_than(6) }
+end
